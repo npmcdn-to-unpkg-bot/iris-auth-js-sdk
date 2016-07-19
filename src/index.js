63be1cb7cbcf5c01466e906e613fb221db11a60d
Robert Sliwa <robjsliwa@gmail.com>
2016-07-19 16:44:34 -0400

    Added remaining API endpoints.

--- src/index.js
+++ src/index.js
@@ -21,6 +21,138 @@ export class AuthManager {
     return response.json();
   }
 
+  mediaRegister(type, mediaToken, successCallback, errorCallback) {
+    return fetch(this.config.managementApiUrl + 'user/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-App-Key': this.config.appKey,
+      },
+      body: JSON.stringify({
+        'Type': type,
+        'MediaToken': mediaToken,
+      }),
+    })
+    .then(this._checkStatus)
+    .then(this._parseJSON)
+    .then((data) => {
+      successCallback(data);
+    })
+    .catch((error) => {
+      console.log('Social media register failed: ' + error);
+      errorCallback(error);
+    });
+  }
+
+  emailRegister(username, email, password, successCallback, errorCallback) {
+    return fetch(this.config.managementApiUrl + 'user/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-App-Key': this.config.appKey,
+      },
+      body: JSON.stringify({
+        'Type': 'Email',
+        'Email': email,
+        'Password': password,
+        'Name': username,
+      }),
+    })
+    .then(this._checkStatus)
+    .then(this._parseJSON)
+    .then((data) => {
+      successCallback(data);
+    })
+    .catch((error) => {
+      console.log('Email register failed: ' + error);
+      errorCallback(error);
+    });
+  }
+
+  userInformation(token, successCallback, errorCallback) {
+    return fetch(this.config.managementApiUrl + 'user/current', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      },
+    })
+    .then(this._checkStatus)
+    .then(this._parseJSON)
+    .then((data) => {
+      successCallback(data);
+    })
+    .catch((error) => {
+      console.log('Get user information failed: ' + error);
+      errorCallback(error);
+    });
+  }
+
+  validateUserAccessToken(token, successCallback, errorCallback) {
+    return fetch(this.config.managementApiUrl + 'user/validate?access_token=' + token, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(this._checkStatus)
+    .then(this._parseJSON)
+    .then((data) => {
+      successCallback(data);
+    })
+    .catch((error) => {
+      console.log('Validate user token failed: ' + error);
+      errorCallback(error);
+    });
+  }
+
+  socialLogin(type, mediaToken, successCallback, errorCallback) {
+    return fetch(this.config.managementApiUrl + 'login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-App-Key': this.config.appKey,
+      },
+      body: JSON.stringify({
+        'Type': type,
+        'MediaToken': mediaToken,
+      }),
+    })
+    .then(this._checkStatus)
+    .then(this._parseJSON)
+    .then((data) => {
+      successCallback(data);
+    })
+    .catch((error) => {
+      console.log('Social media login failed: ' + error);
+      errorCallback(error);
+    });
+  }
+
+  emailLogin(email, password, successCallback, errorCallback) {
+    return fetch(this.config.managementApiUrl + 'login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-App-Key': this.config.appKey,
+      },
+      body: JSON.stringify({
+        'Type': 'Email',
+        'Email': email,
+        'Password': password,
+      }),
+    })
+    .then(this._checkStatus)
+    .then(this._parseJSON)
+    .then((data) => {
+      successCallback(data);
+    })
+    .catch((error) => {
+      console.log('Email login failed: ' + error);
+      errorCallback(error);
+    });
+  }
+
   anonymousLogin(userID, successCallback, errorCallback) {
     return fetch(this.config.managementApiUrl + 'login/anonymous/', {
       method: 'POST',
@@ -39,6 +171,25 @@ export class AuthManager {
     })
     .catch((error) => {
       console.log('Anonymous login failed: ' + error);
+      errorCallback(error);
+    });
+  }
+
+  logout(token, successCallback, errorCallback) {
+    return fetch(this.config.managementApiUrl + 'logout/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+      },
+    })
+    .then(this._checkStatus)
+    .then(this._parseJSON)
+    .then((data) => {
+      successCallback(data);
+    })
+    .catch((error) => {
+      console.log('Logout failed: ' + error);
       errorCallback(error);
     });
   }
